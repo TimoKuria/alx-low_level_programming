a96a14f6201f927d88b57eea08ca7f1b4dc0adc5
TimoKuria <njogutimoh@gmail.com>
2022-10-18 09:18:37 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main: print_alphabet
+ *
+ *Description: lowcase letters
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char lower = 'a';
+
+	while (lower <= 'z')
+	{
+		_putchar(lower);
+		lower++;
+	}
+	_putchar('\n');
+}
